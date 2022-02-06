0d4bdba343db575f916c240e5cfc66692ef1a497
Howard <howardl93@yahoo.com>
2022-02-06 00:20:39 -0800

    style

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,12 +1,32 @@
 // import Cell from './Cell';
 import Row from './Row';
 
+
+const main = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    border: '1px',
+    borderStyle: 'solid',
+    display: 'flex',
+    flexDirection: 'row',
+    margin: '1px'
+}
+
 const Grid =() =>{
+
+    const createRows =()=> {
+        let rows =[];
+        for (let i = 0; i < 5; i++){
+            rows.push(<Row/>)
+        }
+        return rows;
+    };
+
     return(
-        <>
-            <p>This is the grid</p>
-            <Row/>
-        </>
+        <div style={main}>
+            {createRows()}
+        </div>
     )
     
 };
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,19 +1,22 @@
 import Cell from './Cell';
 
 
+
+const cell ={
+    margin: '5px'
+}
 const Row = () =>{
     const cellRows = () =>{
         let row = [];
-        for(let i = 0; i <4; i ++){
-            row.push(<Cell key={i}/>)
+        for(let i = 0; i < 5; i ++){
+            row.push(<Cell style={cell} key={i}/>)
         }
         return row;
     }
     return(
-        <>
+        <div>
             {cellRows()}
-        
-        </>
+        </div>
     )
 }
 
