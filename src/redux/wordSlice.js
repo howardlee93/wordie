fd80d59a0656a1832f59f528314951c11ca21187
howard lee <howardlee@howards-MacBook-Pro.local>
2022-02-07 10:42:45 -0800

    wordSlice

--- src/redux/wordSlice.js
+++ src/redux/wordSlice.js
@@ -4,13 +4,28 @@ export const wordSlice =createSlice({
 
     name:'word',
     initialState:{
-
+        answer:"hello",
+        word:"",
+        correct: false
     },
     reducers:{
+        addLetter(state, action){
+            state.word + action.payload.word;
+        },
+        removeLetter(state, action){
+            state.word - action.payload.word;
+
+        },
+        compareWord(state){
+            return( state.word === state.answer);
+        }
+
 
     }
 
 });
+
+export const { addLetter, removeLetter, compareWord } = wordSlice.actions
 
 export default wordSlice.reducer
 
