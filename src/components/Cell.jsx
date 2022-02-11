ee55bd125e19033e0d4ef1558447044599c54d0d
howard lee <howardlee@howards-MacBook-Pro.local>
2022-02-11 15:30:05 -0800

    adding remaining letters

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Cell = ({letter}) =>{ 
+
     return(
         <div className='w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white'>
             <p>{letter}</p>
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import Cell from './Cell';
+import { MAX_WORD_LENGTH } from '../constant/constant';
 
 const Row = ({guess}) =>{
+
+    const guessLetters = guess.split("");
+    const lettersRemaining = Array.from(Array(MAX_WORD_LENGTH - guessLetters.length));
 
     return(
         <div className="flex flex-row justify-center items-center pb-6">
 
-            {guess ? Array.from(guess).map((letter, i) =>(
+        {Array.from(guess).map((letter, i) =>(
                 <Cell key={i} letter={letter}/>
-            )) 
-            :("")    
-            }
+            )
+        )}
+
+        {lettersRemaining.map((_,i)=>(
+            <Cell key={i}  letter={'x'}/>
+            )
+        )}
+        
         </div>
     )
 }
