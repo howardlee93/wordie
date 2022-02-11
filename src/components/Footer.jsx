b09bee5ee0d81397fc8cbf9836e031b2198d7a32
howard lee <howardlee@howards-MacBook-Pro.local>
2022-02-11 15:42:06 -0800

    adjust footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 export const Footer = () =>{
     return(
       <footer>
-      <p> Copyright Howard Lee {new Date().getFullYear()}</p>
       <div className="pb-6 space-x-2">
         <a href="https://github.com/howardlee93">Github</a>
         <a href="https://www.linkedin.com/in/leehoward93/">LinkedIn</a>
