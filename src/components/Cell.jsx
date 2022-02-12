4c715cc9800e364b3f2daf8774fad050eb5b7b93
Howard <howardl93@yahoo.com>
2022-02-11 23:20:53 -0800

    update dependencies and disabled dark mode for now

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Cell = ({letter}) =>{ 
 
     return(
-        <div className='w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white'>
+        <div className='w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded '>
             <p>{letter}</p>
         </div>
     )
