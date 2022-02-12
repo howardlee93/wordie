f9ab542154954fb08e9022fecb75abc190b3a716
Howard <howardl93@yahoo.com>
2022-02-12 14:23:58 -0800

    add valid guess check

--- src/util/util.js
+++ src/util/util.js
@@ -1,8 +1,15 @@
 import words from '../constant/words';
 
-
-
 export const getAnswer = () =>{
     let randNum = Math.floor(Math.random()* words.length);
     return words[randNum];
+};
+
+export const getValidGuess = (guess) =>{
+    for (let i = 0; i < words.length; i++){
+        if (guess !== words[i]){
+            return false;
+        }
+    }
+    return true;
 }
