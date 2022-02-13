6d791ea0790ccc1d05525246682b029daa483d6e
Howard <howardl93@yahoo.com>
2022-02-12 21:40:39 -0800

    add status char check

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
-const Cell = ({letter}) =>{ 
+const Cell = ({letter, charStatus='absent'}) =>{ 
+    let status;
+
+    switch(charStatus){
+        case 'correct':
+            status = 'text-green-400';
+            break
+        case 'present':
+            status = 'text-yellow-400';
+            break;
+        case 'absent':
+            status = 'text-gray-400	';
+            break
+        default:
+            status = ''
+    }
+
 
     return(
-        <div className='w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded '>
+        <div className={`w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded ${status}`}>
             <p>{letter}</p>
         </div>
     )
--- src/util/status.js
+++ src/util/status.js
@@ -4,7 +4,15 @@ const status =  'absent' | 'present' | 'correct'
 
 
 const getCharStatus = (answer, guess) =>{
+    let statsArr = [];
 
-    
+    for (let i = 0; i < 4 ;i ++){
+        if (guess[i] == answer[i]){
+            statsArr.push(true)
+        }else{
+            push(false);
+        };
+    }
+    return statsArr;
 
 }
